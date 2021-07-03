17529aacb47850af42defedc481502f6594f27de
vinicius <vinisouzax@gmail.com>
2021-07-03 13:00:52 -0300

    Atualizando sql

--- carebr.sql
+++ carebr.sql
@@ -7,7 +7,7 @@ CREATE TABLE `xml` (
   `nNF` varchar(45) DEFAULT NULL,
   `dhEmi` varchar(45) DEFAULT NULL,
   `vNF` varchar(45) DEFAULT NULL,
-  `CPF` varchar(45) DEFAULT NULL,
+  `destCPF` varchar(45) DEFAULT NULL,
   `xNome` varchar(100) DEFAULT NULL,
   `xLgr` varchar(100) DEFAULT NULL,
   `nro` varchar(100) DEFAULT NULL,
@@ -19,5 +19,10 @@ CREATE TABLE `xml` (
   `cPais` varchar(45) DEFAULT NULL,
   `xMun` varchar(45) DEFAULT NULL,
   `nomeXml` varchar(100) DEFAULT NULL,
+  `destCNPJ` varchar(45) DEFAULT NULL,
+  `IE` varchar(45) DEFAULT NULL,
+  `indIEDest` varchar(45) DEFAULT NULL,
+  `email` varchar(45) DEFAULT NULL,
+  `fone` varchar(45) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=47 DEFAULT CHARSET=utf8
+) ENGINE=InnoDB AUTO_INCREMENT=64 DEFAULT CHARSET=utf8
